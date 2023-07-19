893bd41f8c22ade086ca7797784d3c6918a228a1
Yagich <yagich@poto.cafe>
2023-07-19 11:17:56 +0300

    better style lol

--- gradient_label.gd
+++ gradient_label.gd
@@ -1,11 +1,14 @@
 @tool
-class_name GradientLabel extends Label
+extends Label
+class_name GradientLabel
+
 
 func _ready():
 	material = ShaderMaterial.new()
 	material.shader = preload("res://gradient_label.gdshader")
 	resized.connect(self.update_size)
 	self.update_size()
-	
+
+
 func update_size() -> void:
 	material.set_shader_parameter("rect_size", get_global_rect().size)
